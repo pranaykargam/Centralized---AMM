e36f783f5b0ce55d442b5accc653c518bf51f97b
pranaykargam <sunnykargam@gmail.com>
2026-01-04 22:04:48 +0530

    updated README file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 const PORT = 3000;
@@ -8,42 +8,39 @@ app.use(express.json());
 let ETH_BALANCE = 200;
 let USDC_BALANCE = 70000;
 
-app.post("/add-liquidity", (req, res) => {
-
-}); 
+// app.post("/add-liquidity", (req, res) => {});
 
 app.post("/buy-asset", (req, res) => {
-    const quantity = req.body.quantity;
-    const updatedEthQuantity = ETH_BALANCE - quantity; 
-    const updatedUsdcBalance = ETH_BALANCE * USDC_BALANCE / updatedEthQuantity;
-    const paidAmount = updatedUsdcBalance - USDC_BALANCE;
+  const quantity = req.body.quantity;
+  const updatedEthQuantity = ETH_BALANCE - quantity;
+  const updatedUsdcBalance = (ETH_BALANCE * USDC_BALANCE) / updatedEthQuantity;
+  const paidAmount = updatedUsdcBalance - USDC_BALANCE;
 
-    ETH_BALANCE = updatedEthQuantity;
-    USDC_BALANCE = updatedUsdcBalance;
+  ETH_BALANCE = updatedEthQuantity;
+  USDC_BALANCE = updatedUsdcBalance;
 
-    res.json({
-        message: `You paid ${paidAmount} USDC to buy ${quantity} ETH`
-    });
+  res.json({
+    message: `You paid ${paidAmount} USDC to buy ${quantity} ETH`,
+  });
 });
 
 app.post("/sell-asset", (req, res) => {
-    const quantity = req.body.quantity;
-    const updatedEthQuantity = ETH_BALANCE + quantity;
-    const updatedUsdcBalance = ETH_BALANCE * USDC_BALANCE / updatedEthQuantity; 
-    const gottenUsdc = USDC_BALANCE - updatedUsdcBalance; 
+  const quantity = req.body.quantity;
+  const updatedEthQuantity = ETH_BALANCE + quantity;
+  const updatedUsdcBalance = (ETH_BALANCE * USDC_BALANCE) / updatedEthQuantity;
+  const gottenUsdc = USDC_BALANCE - updatedUsdcBalance;
 
-    ETH_BALANCE = updatedEthQuantity;
-    USDC_BALANCE = updatedUsdcBalance;
+  ETH_BALANCE = updatedEthQuantity;
+  USDC_BALANCE = updatedUsdcBalance;
 
-    res.json({
-        message: `You got ${gottenUsdc} USDC for ${quantity} ETH`
-    });
+  res.json({
+    message: `You got ${gottenUsdc} USDC for ${quantity} ETH`,
+  });
 });
 
-app.post("/quote", (req, res) => {
-});
+// app.post("/quote", (req, res) => {});
 
 app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
+  console.log(`Server listening on port ${PORT}`);
 });
 module.exports = app;
